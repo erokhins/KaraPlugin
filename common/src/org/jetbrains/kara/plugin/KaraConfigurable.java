c5efd775149c0e6c2fa9e3093dcaf9864c30b7b5
Erokhin Stanislav <erokhins239@gmail.com>
2013-04-24 18:42:28 +0400

    add configurable dialog

---
+++ common/src/org/jetbrains/kara/plugin/KaraConfigurable.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kara.plugin;
+
+import com.intellij.openapi.options.BeanConfigurable;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.jet.plugin.editor.JetEditorOptions;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class KaraConfigurable extends BeanConfigurable<KaraPluginOptions> implements Configurable {
+
+    protected KaraConfigurable(KaraPluginOptions beanInstance) {
+        super(beanInstance);
+        checkBox("donTShowConversionDialog", "Don't show Html to Kara conversion dialog");
+        checkBox("enableHtmlToKaraConversion", "Enable Html To Kara Conversion");
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "Kara plugin";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return "Settings for Kara plugin";
+    }
+
+    // need for appearance
+    @Override
+    public JComponent createComponent() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JComponent jComponent = super.createComponent();
+        assert jComponent != null;
+        panel.add(jComponent);
+        return panel;
+    }
+}
